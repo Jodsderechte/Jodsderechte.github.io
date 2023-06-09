ebc4d4828640312de5744a58287840a5ca015405
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-06-09 12:54:33 +0200

    Update RequestData.js

--- .github/RequestData.js
+++ .github/RequestData.js
@@ -1,1 +1,20 @@
-
+fetch('https://data.wago.io/lookup/wago?id=RaidAbilityTimeline', {
+		method: 'get',
+		headers: {
+			'Content-type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})
+		.then(response => {
+			if (!response.ok) {
+				throw Error(response.statusText);
+			}
+			return response.json();
+		})
+		.then(data => {
+      console.log(data)
+			data.viewCount
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
