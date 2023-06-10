66c892a5df62e576a34918739e37b3f404873455
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-06-10 09:48:26 +0200

    Update ConvertWaList.sh

--- CrossOriginStuff/ConvertWaList.sh
+++ CrossOriginStuff/ConvertWaList.sh
@@ -2,15 +2,4 @@
 
 #!/bin/bash
 
-# Read and parse the JSON file into a variable
-json_data=$(jq -c '.' CrossOriginStuff/WeakAurasList.json)
-
-# Extract the 'id' values using 'jq' command
-ids=$(echo "$json_data" | jq -r '.hits[].id')
-
-# Convert the extracted IDs to JSON array
-result=$(echo "$ids" | jq -s '.')
-
-# Print the JSON array of IDs
-echo "$result" >> WaList_Converted.json
-
+echo jq -r '.hits[].id' < CrossOriginStuff/WeakAurasList.json > WaList_Converted.json
