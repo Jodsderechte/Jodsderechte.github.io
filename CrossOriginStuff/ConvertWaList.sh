17e9d52cdc542728ede0b8c7d5ba88355659e6c7
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-06-10 10:17:05 +0200

    Update ConvertWaList.sh

--- CrossOriginStuff/ConvertWaList.sh
+++ CrossOriginStuff/ConvertWaList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #!/bin/bash
-echo jq -r '.hits[].id' < CrossOriginStuff/WeakAurasList.json > fetch-api-data-action/WaList_Converted.json
+jq -r '.hits[].id' < CrossOriginStuff/WeakAurasList.json > fetch-api-data-action/WaList_Converted.json
