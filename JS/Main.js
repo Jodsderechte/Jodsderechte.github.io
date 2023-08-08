79a8a16469b45e90d2833077440a574b56ce4245
Jods <39654549+Jodsderechte@users.noreply.github.com>
2023-08-08 18:37:43 +0200

    fix 404 screen

--- JS/Main.js
+++ JS/Main.js
@@ -2,16 +2,6 @@ var mySidebar;
 let WebsiteUrl = "https://florian-reichle.de"
 
 
-window.onload = function(){
-    let form = document.querySelector("#language-picker-select")
-    form.addEventListener("change", function(event){
-      handleLanguage(form.options[form.selectedIndex].lang);
-     })
-     mySidebar = document.getElementById("mySidebar");
-}
-
-
-
 
 // Modal Image Gallery
 function onClick(element) {
@@ -21,21 +11,7 @@ function onClick(element) {
     setTimeout(() => { document.getElementById("modal01").style.display = "block";} ,50);
   }
   
-  
-  // Toggle between showing and hiding the sidebar when clicking the menu icon
-  
-  function w3_open() {
-    if (mySidebar.style.display === 'block') {
-      mySidebar.style.display = 'none';
-    } else {
-      mySidebar.style.display = 'block';
-    }
-  }
-  
-  // Close the sidebar with the close button
-  function w3_close() {
-      mySidebar.style.display = "none";
-  }
+
   const map = [
       { suffix: 'M', threshold: 1e6 },
       { suffix: 'K', threshold: 1e3 },
@@ -55,40 +31,6 @@ function onClick(element) {
     }
   
     return num;
-  }
-  
-  //Language Picker
-  
-  let languageConverter = {
-    "en-US":"english",
-    "de-DE":"deutsch",
-  }
-  let languageCodeConverter ={
-    "english":"en",
-    "deutsch":"de",
-  }
-  let currentLanguage
-  function handleLanguage(language){
-    if(!currentLanguage || language!= currentLanguage){
-      console.log("changing language");
-      if(!languageConverter[language]){
-        language = "en-US";
-      }
-      $.ajax({ url: WebsiteUrl+"/Translations/"+language+".json",dataType: "json", success: function( response ) { 
-        console.log("Language changed to: " + language);
-        document.querySelector("#language-picker-select").value = languageConverter[language];
-        currentLanguage = language;
-        localStorage.setItem('language', language);
-        document.documentElement.setAttribute("lang", language.match(/(.*?)(?=-)/)[1]);
-        document.title = response["WhoAmI"];
-        for (const key in response){
-          var elements = document.getElementsByClassName(key);
-          for(var i = 0; i < elements.length; i++) {
-            elements[i].innerHTML = response[key];
-          }
-        }
-    }})  
-    }
   }
   
   // Auto generation of Dungeon Aura Entries
@@ -232,7 +174,5 @@ function onClick(element) {
   
       }
     }})
-    let language = localStorage.getItem('language') || navigator.language || navigator.userLanguage || 'en-US';
-    handleLanguage(language); //setting language
   })
   
