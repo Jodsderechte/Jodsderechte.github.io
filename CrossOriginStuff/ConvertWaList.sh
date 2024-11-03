56ff01845f29803f866b2e81fa161f6dd8ec1d10
Florian Reichle <39654549+Jodsderechte@users.noreply.github.com>
2024-11-03 18:35:56 +0100

    Update ConvertWaList.sh

--- CrossOriginStuff/ConvertWaList.sh
+++ CrossOriginStuff/ConvertWaList.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #!/bin/bash
-curl -s 'https://data.wago.io/search/es?q=User%3A%22Jodsderechte%22&mode=wow&page=0&sort=' | jq -cr '"content=\([.hits[].id])"' > fetch-api-data-action/WaList_Converted.json
+curl -s 'https://data.wago.io/search/es?q=User%3A%22Jodsderechte%22&mode=wow&page=0&sort=' | jq -cr '"content=\([.hits[].id])"' > Data/WaList_Converted.json
