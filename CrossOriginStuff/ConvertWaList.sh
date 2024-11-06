906a37785b9c449ea34a81c419c2c99126e6272d
Jodsderechte <39654549+Jodsderechte@users.noreply.github.com>
2024-11-06 16:33:45 +0100

    more fixes

--- CrossOriginStuff/ConvertWaList.sh
+++ CrossOriginStuff/ConvertWaList.sh
@@ -7,21 +7,16 @@ output_file="Data/WaList_Converted.json"
 # Get total number of results
 total_results=$(curl -s "$url&page=0" | jq '.total')
 
- # If total_results is empty, abort
+# If total_results is empty, abort
 if [ -z "$total_results" ]; then
   echo "Error: Total results not found or invalid response"
-  exit 5
+  exit 1
 fi
 
 # Calculate total pages
 total_pages=$(( (total_results + page_size - 1) / page_size )) # This rounds up
+
 echo "Checking for a total of: $total_pages pages"
-
-# Calculate total pages
-total_pages=$(( (total_results + page_size - 1) / page_size )) # This rounds up
-
-# Initialize empty JSON array to hold the results
-echo "[]" > "$output_file"
 
 # Initialize empty JSON array to hold the results
 results="[]"
@@ -40,5 +35,6 @@ unique_results=$(echo "$results" | jq 'unique')
 # Debugging: Output the raw JSON to make sure it's correct
 echo "Unique results: $unique_results"
 
-echo "$unique_results" > "$output_file"
+# Set the unique results as an environment variable
+echo "content=$(echo "$unique_results" | jq -cr .)" >> "$output_file"
 echo "Data written to $output_file"
